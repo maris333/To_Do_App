cbf93e99b928a85834d34915546bb960b1de3fd6
maris333 <animaral@gmail.com>
2022-11-18 10:44:39 +0100

    Create to do list

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-def pretty_table():
-    return "table"
+def to_do_list():
+    return []
+
+
+def add_to_list(to_do_list, item):
+    to_do_list.append(item)
 
 
 if __name__ == '__main__':
-    print(pretty_table())
+    my_list = to_do_list()
+    add_to_list(my_list, "item1")
+    add_to_list(my_list, "item2")
+    for item in my_list:
+        print(item)
+
 
 
